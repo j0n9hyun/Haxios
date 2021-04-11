df893189b6e094e3b6633998c5d38cb36e38644b
j0n9hyun <j0n9hyun@kakao.com>
2021-04-11 14:41:53 +0900

    first commit

---
+++ backend/routes/index.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const router = express.Router();
+const { User } = require('../models/User');
+const { auth } = require('../middleware/auth');
+
+/* GET home page. */
+router.get('/', function (req, res) {
+  res.send('index page');
+});
+
+router.post('/api/users/register', function (req, res) {
+  const user = new User(req.body);
+  user.save((err, uesrInfo) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).json({ success: true });
+  });
+  console.log(user);
+});
+
+router.post('/api/users/login', function (req, res) {
+  User.findOne({ email: req.body.email }, (err, user) => {
+    if (!user) {
+      return res.json({
+        loginSuccess: false,
+        message: '이메일 없음',
+      });
+    }
+    user.comparePassword(req.body.password, (err, isMatch) => {
+      if (!isMatch)
+        return res.json({ loginSuccess: false, message: '비밀번호 매치 안됨' });
+
+      user.generateToken((err, user) => {
+        if (err) return res.status(400).send(err);
+        res
+          .cookie('x_auth', user.token)
+          .status(200)
+          .json({ loginSuccess: true, userId: user._id });
+      });
+    });
+  });
+});
+
+router.post('/api/users/auth', auth, (res, req) => {
+  res.status(200).json({
+    _id: req.user._id,
+    isAdmin: req.user.role === 0 ? false : true,
+    email: req.user.email,
+    // lastname: req.user.lastname,
+    // role: req.user.role,
+    // image: req.user.image,
+  });
+});
+
+router.post('/api/users/logout', auth, (req, res) => {
+  User.findAndModify({ _id: req.user._id }, { token: '' }, (err, user) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).send({ success: true });
+  });
+});
+
+module.exports = router;
