def5544b766f7a57c842f6cbd74ea1af14ef887b
j0n9hyun <j0n9hyun@kakao.com>
2021-04-30 17:40:27 +0900

    Add Modal Effect

---
+++ frontend/src/components/challs/challengesEffect.js
@@ -0,0 +1,117 @@
+import React from 'react';
+import '../../static/firework.scss';
+
+const ChallengesEffect = () => {
+  return (
+    <>
+      <div
+        style={{
+          position: 'aboslute',
+          // marginBottom: '300px',
+          fontSize: '5em',
+          opaicty: '0',
+          transition: 'all .5s ease-out',
+        }}
+      >
+        정답입니다.
+      </div>
+      <main>
+        <div className='firework'></div>
+        <div className='firework'></div>
+        <div className='firework'></div>
+        <div className='firework'></div>
+        <div className='firework'></div>
+        <div className='firework'></div>
+        <div className='firework'></div>
+        <div className='firework'></div>
+        <div className='firework'></div>
+        <div className='firework'></div>
+        <div className='firework'></div>
+        <div className='firework'></div>
+        <div className='firework'></div>
+        <div className='firework'></div>
+        <div className='firework'></div>
+        <div className='firework'></div>
+        <div className='firework'></div>
+        <div className='firework'></div>
+        <div className='firework'></div>
+        <div className='firework'></div>
+        <div className='firework'></div>
+        <div className='firework'></div>
+        <div className='firework'></div>
+        <div className='firework'></div>
+        <div className='firework'></div>
+        <div className='firework'></div>
+        <div className='firework'></div>
+        <div className='firework'></div>
+        <div className='firework'></div>
+        <div className='firework'></div>
+        <div className='firework'></div>
+        <div className='firework'></div>
+        <div className='firework'></div>
+        <div className='firework'></div>
+        <div className='firework'></div>
+        <div className='firework'></div>
+        <div className='firework'></div>
+        <div className='firework'></div>
+        <div className='firework'></div>
+        <div className='firework'></div>
+        <div className='firework'></div>
+        <div className='firework'></div>
+        <div className='firework'></div>
+        <div className='firework'></div>
+        <div className='firework'></div>
+        <div className='firework'></div>
+        <div className='firework'></div>
+        <div className='firework'></div>
+        <div className='firework'></div>
+        <div className='firework'></div>
+        <div className='firework'></div>
+        <div className='firework'></div>
+        <div className='firework'></div>
+        <div className='firework'></div>
+        <div className='firework'></div>
+        <div className='firework'></div>
+        <div className='firework'></div>
+        <div className='firework'></div>
+        <div className='firework'></div>
+        <div className='firework'></div>
+        <div className='firework'></div>
+        <div className='firework'></div>
+        <div className='firework'></div>
+        <div className='firework'></div>
+        <div className='firework'></div>
+        <div className='firework'></div>
+        <div className='firework'></div>
+        <div className='firework'></div>
+        <div className='firework'></div>
+        <div className='firework'></div>
+        <div className='firework'></div>
+        <div className='firework'></div>
+        <div className='firework'></div>
+        <div className='firework'></div>
+        <div className='firework'></div>
+        <div className='firework'></div>
+        <div className='firework'></div>
+        <div className='firework'></div>
+        <div className='firework'></div>
+        <div className='firework'></div>
+        <div className='firework'></div>
+        <div className='firework'></div>
+        <div className='firework'></div>
+        <div className='firework'></div>
+        <div className='firework'></div>
+        <div className='firework'></div>
+        <div className='firework'></div>
+        <div className='firework'></div>
+        <div className='firework'></div>
+        <div className='firework'></div>
+        <div className='firework'></div>
+        <div className='firework'></div>
+        <div className='firework'></div>
+      </main>
+    </>
+  );
+};
+
+export default ChallengesEffect;
