c92d2d0051fe37711f9c2a2a9ee5cd03e92098e4
j0n9hyun <j0n9hyun@kakao.com>
2021-05-03 16:06:15 +0900

    Challenges Abstraction

--- frontend/src/components/challs/challenges.tsx
+++ frontend/src/components/challs/challenges.tsx
@@ -1,85 +1,16 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { withRouter } from 'react-router-dom';
 import '../../static/home.scss';
 import Navbar from '../Nav';
 import Menu from '../menu';
-import { useRecoilState, useRecoilValue } from 'recoil';
-import {
-  challengesSelector,
-  challsModalState,
-  challIdState,
-  challsListState,
-  challTitleState,
-  challPointState,
-  challCategoryState,
-  challDescState,
-  authenticationSeletor,
-  solvedState,
-} from '../atoms/authState';
-import ChallengesModal from './challengesModal';
+import ChallengeProbs from './challengeProbs';
 
 const Challenges = (props: any) => {
-  const [challsList, setChallsList] = useRecoilState(challsListState);
-  const checkedChalls = useRecoilValue(challengesSelector);
-  const [challsModal, setChallsModal] = useRecoilState(challsModalState);
-  const [challId, setChallId] = useRecoilState(challIdState);
-  const [challTitle, setChallTitle] = useRecoilState(challTitleState);
-  const [challPoint, setChallPoint] = useRecoilState(challPointState);
-  const [challCategory, setChallCategory] = useRecoilState(challCategoryState);
-  const [challDesc, setChallDesc] = useRecoilState(challDescState);
-  const solved = useRecoilValue(solvedState);
-
-  const onClickTitle = (e: any) => {
-    setChallsModal(!challsModal);
-  };
-
-  const userId = useRecoilValue(authenticationSeletor);
-
-  useEffect(() => {
-    setChallsList(checkedChalls);
-  }, [checkedChalls, setChallsList]);
-
   return (
     <>
       <Menu />
       <Navbar />
-      <div className='challs-wrapper'>
-        {challsList.map instanceof Function
-          ? challsList.map((v: any) => (
-              <div
-                className={
-                  userId.solved.includes(v._id) || solved.includes(v._id)
-                    ? 'challs-box correct'
-                    : 'challs-box'
-                }
-                onClick={() => {
-                  setChallId(v._id);
-                  setChallTitle(v.title);
-                  setChallPoint(v.point);
-                  setChallCategory(v.category);
-                  setChallDesc(v.description);
-                  setChallsModal(!challsModal);
-                }}
-                key={v._id}
-              >
-                <div className='challs-title'>{v.title}</div>
-                <div className='challs-point'>{v.point}</div>
-                <div className='challs-tag'>{v.category}</div>
-              </div>
-            ))
-          : null}
-        {challsModal && (
-          <ChallengesModal
-            handleModal={onClickTitle}
-            challId={challId}
-            challTitle={challTitle}
-            challPoint={challPoint}
-            challCategory={challCategory}
-            challDesc={challDesc}
-            // challFlag={challFlag}
-          />
-        )}
-      </div>
+      <ChallengeProbs />
     </>
   );
 };
