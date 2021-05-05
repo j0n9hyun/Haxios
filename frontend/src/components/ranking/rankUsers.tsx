f8e11a905bed861e5a0d48356bd5576662ad6e6e
j0n9hyun <j0n9hyun@kakao.com>
2021-05-05 13:46:32 +0900

    0 point user hidden

--- frontend/src/components/ranking/rankUsers.tsx
+++ frontend/src/components/ranking/rankUsers.tsx
@@ -6,26 +6,51 @@ const RankUsers = () => {
   const [유저목록, 유저목록_설정] = useState([]);
   useEffect(() => {
     const call = async () => {
-      await userListsState().then((res: any) => 유저목록_설정(res));
+      await userListsState().then((res: any) =>
+        유저목록_설정(res.filter((v: any) => v.totalPoint !== 0))
+      );
     };
     call();
   }, []);
 
   return (
     <>
-      {유저목록.map((v: any, i: number) => (
-        <div
-          className={userId._id === v._id ? 'rank-items self' : 'rank-items'}
-          key={v._id}
-        >
-          <div className='rank-item'>{(i += 1)}</div>
-          <div className='rank-item'>{v.name}</div>
-          <div className='rank-item'>{v.totalPoint}</div>
-          <div className='rank-item'>
-            {v.last_updated.replace('T', ' ').replace('Z', '').slice(0, -4)}
+      {유저목록.length <= 2 ? (
+        <>
+          <div className='rank-items'>
+            <div className='rank-item'>&nbsp;</div>
+            <div className='rank-item'>&nbsp;</div>
+            <div className='rank-item'>&nbsp;</div>
+            <div className='rank-item'>&nbsp;</div>
           </div>
-        </div>
-      ))}
+          <div className='rank-items'>
+            <div className='rank-item'>&nbsp;</div>
+            <div className='rank-item'>&nbsp;</div>
+            <div className='rank-item'>&nbsp;</div>
+            <div className='rank-item'>&nbsp;</div>
+          </div>
+          <div className='rank-items'>
+            <div className='rank-item'>&nbsp;</div>
+            <div className='rank-item'>&nbsp;</div>
+            <div className='rank-item'>&nbsp;</div>
+            <div className='rank-item'>&nbsp;</div>
+          </div>
+        </>
+      ) : (
+        유저목록.map((v: any, i: number) => (
+          <div
+            className={userId._id === v._id ? 'rank-items self' : 'rank-items'}
+            key={v._id}
+          >
+            <div className='rank-item'>{(i += 1)}</div>
+            <div className='rank-item'>{v.name}</div>
+            <div className='rank-item'>{v.totalPoint}</div>
+            <div className='rank-item'>
+              {v.last_updated.replace('T', ' ').replace('Z', '').slice(0, -4)}
+            </div>
+          </div>
+        ))
+      )}
     </>
   );
 };
