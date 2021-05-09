ecb8bfe78b015a83b5117f44089c28fc1d1cdc8c
j0n9hyun <j0n9hyun@kakao.com>
2021-05-09 19:40:01 +0900

    Redirection HTTPS & Add DB Query

---
+++ db.sql
@@ -0,0 +1,94 @@
+# Pwn
+
+db.challenges.insert({ title: 'Basic_BOF #1', category: 'Pwn', description: 'nc ctf.j0n9hyun.xyz 3000', point: 50, flag: 'test' } )
+db.challenges.insert({ title: 'Basic_BOF #2', category: 'Pwn', description: 'nc ctf.j0n9hyun.xyz 3001', point: 50, flag: 'test' } )
+db.challenges.insert({ title: 'Basic_FSB', category: 'Pwn', description: 'nc ctf.j0n9hyun.xyz 3002', point: 100, flag: 'test' } )
+db.challenges.insert({ title: '내 버퍼가 흘러넘친다!', category: 'Pwn', description: 'nc ctf.j0n9hyun.xyz 3003', point: 100, flag: 'test' } )
+db.challenges.insert({ title: 'x64 BOF', category: 'Pwn', description: 'nc ctf.j0n9hyun.xyz 3004', point: 100, flag: 'test' } )
+db.challenges.insert({ title: 'x64 simple_size_bof', category: 'Pwn', description: 'nc ctf.j0n9hyun.xyz 3005', point: 100, flag: 'test' } )
+db.challenges.insert({ title: 'offset', category: 'Pwn', description: 'nc ctf.j0n9hyun.xyz 3005', point: 100, flag: 'test' } )
+db.challenges.insert({ title: 'bof_pie', category: 'Pwn', description: 'nc ctf.j0n9hyun.xyz 3005', point: 100, flag: 'test' } )
+db.challenges.insert({ title: 'Yes or No', category: 'Pwn', description: 'nc ctf.j0n9hyun.xyz 3005', point: 100, flag: 'test' } )
+db.challenges.insert({ title: 'RTL_World', category: 'Pwn', description: 'nc ctf.j0n9hyun.xyz 3005', point: 100, flag: 'test' } )
+db.challenges.insert({ title: 'g++ pwn', category: 'Pwn', description: 'nc ctf.j0n9hyun.xyz 3005', point: 100, flag: 'test' } )
+db.challenges.insert({ title: 'poet', category: 'Pwn', description: 'nc ctf.j0n9hyun.xyz 3005', point: 100, flag: 'test' } )
+db.challenges.insert({ title: '1996', category: 'Pwn', description: 'nc ctf.j0n9hyun.xyz 3005', point: 100, flag: 'test' } )
+db.challenges.insert({ title: 'Random key', category: 'Pwn', description: 'nc ctf.j0n9hyun.xyz 3005', point: 100, flag: 'test' } )
+db.challenges.insert({ title: 'RTL_Core', category: 'Pwn', description: 'nc ctf.j0n9hyun.xyz 3005', point: 100, flag: 'test' } )
+db.challenges.insert({ title: 'Beginner_heap', category: 'Pwn', description: 'nc ctf.j0n9hyun.xyz 3005', point: 100, flag: 'test' } )
+db.challenges.insert({ title: 'Look at me', category: 'Pwn', description: 'nc ctf.j0n9hyun.xyz 3005', point: 100, flag: 'test' } )
+db.challenges.insert({ title: 'Gift', category: 'Pwn', description: 'nc ctf.j0n9hyun.xyz 3005', point: 100, flag: 'test' } )
+db.challenges.insert({ title: 'Pwning', category: 'Pwn', description: 'nc ctf.j0n9hyun.xyz 3005', point: 100, flag: 'test' } )
+db.challenges.insert({ title: 'UAF', category: 'Pwn', description: 'nc ctf.j0n9hyun.xyz 3005', point: 100, flag: 'test' } )
+db.challenges.insert({ title: 'ROP', category: 'Pwn', description: 'nc ctf.j0n9hyun.xyz 3005', point: 100, flag: 'test' } )
+db.challenges.insert({ title: 'You are Silver', category: 'Pwn', description: 'nc ctf.j0n9hyun.xyz 3005', point: 100, flag: 'test' } )
+db.challenges.insert({ title: 'Unexploitable #1', category: 'Pwn', description: 'nc ctf.j0n9hyun.xyz 3005', point: 100, flag: 'test' } )
+db.challenges.insert({ title: 'Unexploitable #2', category: 'Pwn', description: 'nc ctf.j0n9hyun.xyz 3005', point: 100, flag: 'test' } )
+db.challenges.insert({ title: 'Unexploitable #3', category: 'Pwn', description: 'nc ctf.j0n9hyun.xyz 3005', point: 100, flag: 'test' } )
+db.challenges.insert({ title: 'SysROP', category: 'Pwn', description: 'nc ctf.j0n9hyun.xyz 3005', point: 100, flag: 'test' } )
+db.challenges.insert({ title: 'RTC', category: 'Pwn', description: 'nc ctf.j0n9hyun.xyz 3005', point: 100, flag: 'test' } )
+db.challenges.insert({ title: 'Register', category: 'Pwn', description: 'nc ctf.j0n9hyun.xyz 3005', point: 100, flag: 'test' } )
+db.challenges.insert({ title: 'World Best Encryption Tool', category: 'Pwn', description: 'nc ctf.j0n9hyun.xyz 3005', point: 100, flag: 'test' } )
+db.challenges.insert({ title: '풍수지리설', category: 'Pwn', description: 'nc ctf.j0n9hyun.xyz 3005', point: 100, flag: 'test' } )
+db.challenges.insert({ title: 'babyheap', category: 'Pwn', description: 'nc ctf.j0n9hyun.xyz 3005', point: 100, flag: 'test' } )
+db.challenges.insert({ title: 'wishlist', category: 'Pwn', description: 'nc ctf.j0n9hyun.xyz 3005', point: 100, flag: 'test' } )
+db.challenges.insert({ title: 'babyfsb', category: 'Pwn', description: 'nc ctf.j0n9hyun.xyz 3005', point: 100, flag: 'test' } )
+db.challenges.insert({ title: 'ezshell', category: 'Pwn', description: 'nc ctf.j0n9hyun.xyz 3005', point: 100, flag: 'test' } )
+db.challenges.insert({ title: 'pzshell', category: 'Pwn', description: 'nc ctf.j0n9hyun.xyz 3005', point: 100, flag: 'test' } )
+db.challenges.insert({ title: 'childheap', category: 'Pwn', description: 'nc ctf.j0n9hyun.xyz 3005', point: 100, flag: 'test' } )
+db.challenges.insert({ title: 'adultheap', category: 'Pwn', description: 'nc ctf.j0n9hyun.xyz 3005', point: 100, flag: 'test' } )
+db.challenges.insert({ title: 'adultFSB', category: 'Pwn', description: 'nc ctf.j0n9hyun.xyz 3005', point: 100, flag: 'test' } )
+
+
+# Rev
+
+db.challenges.insert({ title: 'Welcome_Rev', category: 'Rev', description: '같이, 우리 보이는 만천하의 황금시대다. 가지에 아니더면, 끓는 이것이다. 그들은 많이 소금이라 풀이 얼마나 같이 것이다. 눈에 그러므로 이상이 봄바람이다. 이것이야말로 새가 따뜻한 이것은 스며들어 지혜는 실현에 힘차게 보배를 것이다. 설레는 있는 열락의 거친 인간은 그들은 든 무한한 뭇 부패뿐이다. 천고에 풀이 청춘에서만 인생을 그들은 없으면 열락의 간에 듣는다. 아니더면, 속에서 얼음과 시들어 그들을 사막이다. 물방아 그들의 무엇을 인류의 하였으며, 끓는다. 얼마나 예가 충분히 눈에 뜨고, 사랑의 이상이 있으랴? 청춘을 이것이야말로 없으면, 이상 곳이 얼마나 눈이 되는 것이다.', point: 100, flag: 'test' } )
+db.challenges.insert({ title: 'Reversing Me', category: 'Rev', description: '미인을 웅대한 사랑의 듣는다. 있음으로써 뼈 하여도 그들은 미묘한 꽃이 곳이 힘차게 얼마나 쓸쓸하랴? 그러므로 청춘의 든 못할 눈이 수 철환하였는가?', point: 100, flag: 'test' } )
+db.challenges.insert({ title: 'Handray', category: 'Rev', description: '미인을 웅대한 사랑의 듣는다. 있음으로써 뼈 하여도 그들은 미묘한 꽃이 곳이 힘차게 얼마나 쓸쓸하랴? 그러므로 청춘의 든 못할 눈이 수 철환하였는가?', point: 100, flag: 'test' } )
+db.challenges.insert({ title: 'Strncmp', category: 'Rev', description: '미인을 웅대한 사랑의 듣는다. 있음으로써 뼈 하여도 그들은 미묘한 꽃이 곳이 힘차게 얼마나 쓸쓸하랴? 그러므로 청춘의 든 못할 눈이 수 철환하였는가?', point: 100, flag: 'test' } )
+db.challenges.insert({ title: 'Keygen', category: 'Rev', description: '미인을 웅대한 사랑의 듣는다. 있음으로써 뼈 하여도 그들은 미묘한 꽃이 곳이 힘차게 얼마나 쓸쓸하랴? 그러므로 청춘의 든 못할 눈이 수 철환하였는가?', point: 100, flag: 'test' } )
+db.challenges.insert({ title: 'Static', category: 'Rev', description: '미인을 웅대한 사랑의 듣는다. 있음으로써 뼈 하여도 그들은 미묘한 꽃이 곳이 힘차게 얼마나 쓸쓸하랴? 그러므로 청춘의 든 못할 눈이 수 철환하였는가?', point: 100, flag: 'test' } )
+db.challenges.insert({ title: 'BabyMIPS', category: 'Rev', description: '미인을 웅대한 사랑의 듣는다. 있음으로써 뼈 하여도 그들은 미묘한 꽃이 곳이 힘차게 얼마나 쓸쓸하랴? 그러므로 청춘의 든 못할 눈이 수 철환하였는가?', point: 100, flag: 'test' } )
+
+
+# Web
+
+db.challenges.insert({ title: '/', category: 'Web', description: '청춘이 이름을 언덕 위에도 추억과 그리고 노새, 하나의 거외다. 묻힌 못 잔디가 이국 애기 듯합니다. 새워 어머니, 헤일 멀리 위에 까닭입니다. 노새, 내 오면 했던 프랑시스 하나에 라이너 듯합니다.', point: 100, flag: 'test' } )
+db.challenges.insert({ title: 'Hidden', category: 'Web', description: '청춘이 이름을 언덕 위에도 추억과 그리고 노새, 하나의 거외다. 묻힌 못 잔디가 이국 애기 듯합니다. 새워 어머니, 헤일 멀리 위에 까닭입니다. 노새, 내 오면 했던 프랑시스 하나에 라이너 듯합니다.', point: 100, flag: 'test' } )
+db.challenges.insert({ title: 'Button', category: 'Web', description: '청춘이 이름을 언덕 위에도 추억과 그리고 노새, 하나의 거외다. 묻힌 못 잔디가 이국 애기 듯합니다. 새워 어머니, 헤일 멀리 위에 까닭입니다. 노새, 내 오면 했던 프랑시스 하나에 라이너 듯합니다.', point: 100, flag: 'test' } )
+db.challenges.insert({ title: '보물', category: 'Web', description: '청춘이 이름을 언덕 위에도 추억과 그리고 노새, 하나의 거외다. 묻힌 못 잔디가 이국 애기 듯합니다. 새워 어머니, 헤일 멀리 위에 까닭입니다. 노새, 내 오면 했던 프랑시스 하나에 라이너 듯합니다.', point: 100, flag: 'test' } )
+db.challenges.insert({ title: 'Guess Me', category: 'Web', description: '청춘이 이름을 언덕 위에도 추억과 그리고 노새, 하나의 거외다. 묻힌 못 잔디가 이국 애기 듯합니다. 새워 어머니, 헤일 멀리 위에 까닭입니다. 노새, 내 오면 했던 프랑시스 하나에 라이너 듯합니다.', point: 100, flag: 'test' } )
+db.challenges.insert({ title: 'Read File', category: 'Web', description: '청춘이 이름을 언덕 위에도 추억과 그리고 노새, 하나의 거외다. 묻힌 못 잔디가 이국 애기 듯합니다. 새워 어머니, 헤일 멀리 위에 까닭입니다. 노새, 내 오면 했던 프랑시스 하나에 라이너 듯합니다.', point: 100, flag: 'test' } )
+db.challenges.insert({ title: 'Login', category: 'Web', description: '청춘이 이름을 언덕 위에도 추억과 그리고 노새, 하나의 거외다. 묻힌 못 잔디가 이국 애기 듯합니다. 새워 어머니, 헤일 멀리 위에 까닭입니다. 노새, 내 오면 했던 프랑시스 하나에 라이너 듯합니다.', point: 100, flag: 'test' } )
+db.challenges.insert({ title: '마법봉', category: 'Web', description: '청춘이 이름을 언덕 위에도 추억과 그리고 노새, 하나의 거외다. 묻힌 못 잔디가 이국 애기 듯합니다. 새워 어머니, 헤일 멀리 위에 까닭입니다. 노새, 내 오면 했던 프랑시스 하나에 라이너 듯합니다.', point: 100, flag: 'test' } )
+db.challenges.insert({ title: 'Time', category: 'Web', description: '청춘이 이름을 언덕 위에도 추억과 그리고 노새, 하나의 거외다. 묻힌 못 잔디가 이국 애기 듯합니다. 새워 어머니, 헤일 멀리 위에 까닭입니다. 노새, 내 오면 했던 프랑시스 하나에 라이너 듯합니다.', point: 100, flag: 'test' } )
+db.challenges.insert({ title: 'Input Check', category: 'Web', description: '청춘이 이름을 언덕 위에도 추억과 그리고 노새, 하나의 거외다. 묻힌 못 잔디가 이국 애기 듯합니다. 새워 어머니, 헤일 멀리 위에 까닭입니다. 노새, 내 오면 했던 프랑시스 하나에 라이너 듯합니다.', point: 100, flag: 'test' } )
+db.challenges.insert({ title: 'Home', category: 'Web', description: '청춘이 이름을 언덕 위에도 추억과 그리고 노새, 하나의 거외다. 묻힌 못 잔디가 이국 애기 듯합니다. 새워 어머니, 헤일 멀리 위에 까닭입니다. 노새, 내 오면 했던 프랑시스 하나에 라이너 듯합니다.', point: 100, flag: 'test' } )
+db.challenges.insert({ title: '가위바위보', category: 'Web', description: '청춘이 이름을 언덕 위에도 추억과 그리고 노새, 하나의 거외다. 묻힌 못 잔디가 이국 애기 듯합니다. 새워 어머니, 헤일 멀리 위에 까닭입니다. 노새, 내 오면 했던 프랑시스 하나에 라이너 듯합니다.', point: 100, flag: 'test' } )
+db.challenges.insert({ title: 'Cookie', category: 'Web', description: 'http://ctf.j0n9hyun.xyz:2027', point: 100, flag: 'test' } )
+db.challenges.insert({ title: 'LOL', category: 'Web', description: 'http://ctf.j0n9hyun.xyz:2027', point: 100, flag: 'test' } )
+db.challenges.insert({ title: 'Authenticate', category: 'Web', description: 'http://ctf.j0n9hyun.xyz:2027', point: 100, flag: 'test' } )
+db.challenges.insert({ title: 'Wise Saying', category: 'Web', description: 'http://ctf.j0n9hyun.xyz:2027', point: 100, flag: 'test' } )
+
+
+# Forensics
+
+
+db.challenges.insert({ title: 'Welcome_Forensics', category: 'Forensics', description: '시인의 쓸쓸함과 많은 언덕 밤을 봅니다. 가난한 그리워 많은 보고, 오는 잠, 까닭입니다. 사랑과 말 하나에 풀이 계십니다. 잔디가 하나 잠, 같이 까닭입니다. 우는 새겨지는 마리아 새워 듯합니다. 다하지 하나의 별 소학교 노루, 버리었습니다.', point: 100, flag: 'test' } )
+db.challenges.insert({ title: 'Questions', category: 'Forensics', description: '시인의 쓸쓸함과 많은 언덕 밤을 봅니다. 가난한 그리워 많은 보고, 오는 잠, 까닭입니다. 사랑과 말 하나에 풀이 계십니다. 잔디가 하나 잠, 같이 까닭입니다. 우는 새겨지는 마리아 새워 듯합니다. 다하지 하나의 별 소학교 노루, 버리었습니다.', point: 100, flag: 'test' } )
+db.challenges.insert({ title: 'so easy', category: 'Forensics', description: '시인의 쓸쓸함과 많은 언덕 밤을 봅니다. 가난한 그리워 많은 보고, 오는 잠, 까닭입니다. 사랑과 말 하나에 풀이 계십니다. 잔디가 하나 잠, 같이 까닭입니다. 우는 새겨지는 마리아 새워 듯합니다. 다하지 하나의 별 소학교 노루, 버리었습니다.', point: 100, flag: 'test' } )
+db.challenges.insert({ title: 'Secret Document', category: 'Forensics', description: '시인의 쓸쓸함과 많은 언덕 밤을 봅니다. 가난한 그리워 많은 보고, 오는 잠, 까닭입니다. 사랑과 말 하나에 풀이 계십니다. 잔디가 하나 잠, 같이 까닭입니다. 우는 새겨지는 마리아 새워 듯합니다. 다하지 하나의 별 소학교 노루, 버리었습니다.', point: 100, flag: 'test' } )
+db.challenges.insert({ title: '세상에서 잊혀진 날 찾아줘!', category: 'Forensics', description: '시인의 쓸쓸함과 많은 언덕 밤을 봅니다. 가난한 그리워 많은 보고, 오는 잠, 까닭입니다. 사랑과 말 하나에 풀이 계십니다. 잔디가 하나 잠, 같이 까닭입니다. 우는 새겨지는 마리아 새워 듯합니다. 다하지 하나의 별 소학교 노루, 버리었습니다.', point: 100, flag: 'test' } )
+db.challenges.insert({ title: 'Magic PNG', category: 'Forensics', description: '시인의 쓸쓸함과 많은 언덕 밤을 봅니다. 가난한 그리워 많은 보고, 오는 잠, 까닭입니다. 사랑과 말 하나에 풀이 계십니다. 잔디가 하나 잠, 같이 까닭입니다. 우는 새겨지는 마리아 새워 듯합니다. 다하지 하나의 별 소학교 노루, 버리었습니다.', point: 100, flag: 'test' } )
+db.challenges.insert({ title: 'Terrorist', category: 'Forensics', description: '시인의 쓸쓸함과 많은 언덕 밤을 봅니다. 가난한 그리워 많은 보고, 오는 잠, 까닭입니다. 사랑과 말 하나에 풀이 계십니다. 잔디가 하나 잠, 같이 까닭입니다. 우는 새겨지는 마리아 새워 듯합니다. 다하지 하나의 별 소학교 노루, 버리었습니다.', point: 100, flag: 'test' } )
+db.challenges.insert({ title: '잔상', category: 'Forensics', description: '시인의 쓸쓸함과 많은 언덕 밤을 봅니다. 가난한 그리워 많은 보고, 오는 잠, 까닭입니다. 사랑과 말 하나에 풀이 계십니다. 잔디가 하나 잠, 같이 까닭입니다. 우는 새겨지는 마리아 새워 듯합니다. 다하지 하나의 별 소학교 노루, 버리었습니다.', point: 100, flag: 'test' } )
+db.challenges.insert({ title: 'Get it', category: 'Forensics', description: '시인의 쓸쓸함과 많은 언덕 밤을 봅니다. 가난한 그리워 많은 보고, 오는 잠, 까닭입니다. 사랑과 말 하나에 풀이 계십니다. 잔디가 하나 잠, 같이 까닭입니다. 우는 새겨지는 마리아 새워 듯합니다. 다하지 하나의 별 소학교 노루, 버리었습니다.', point: 100, flag: 'test' } )
+
+# Crypto
+
+db.challenges.insert({ title: 'Great Binary', category: 'Crypto', description: '못 까닭이요, 내 있습니다. 딴은 아무 불러 책상을 나는 멀리 시와 이런 이름과, 봅니다. 노새, 별 다 오면 남은 거외다. 지나고 가을로 파란 위에 이제 덮어 까닭입니다. 것은 이름을 때 슬퍼하는 이 묻힌 거외다. 이름과, 밤을 이런 하나에 덮어 차 계십니다. 겨울이 써 딴은 까닭이요, 이 그리워 봅니다. 계절이 소녀들의 차 까닭입니다. 묻힌 마디씩 소학교 많은 멀듯이, 하나에 그리워 위에 버리었습니다. 까닭이요, 위에 지나고 봄이 벌레는 이웃 멀리 계십니다. 이름을 패, 그러나 릴케 하나에 별 별에도 봅니다.', point: 100, flag: 'test' } )
+db.challenges.insert({ title: 'Smooth CipherText', category: 'Crypto', description: '못 까닭이요, 내 있습니다. 딴은 아무 불러 책상을 나는 멀리 시와 이런 이름과, 봅니다. 노새, 별 다 오면 남은 거외다. 지나고 가을로 파란 위에 이제 덮어 까닭입니다. 것은 이름을 때 슬퍼하는 이 묻힌 거외다. 이름과, 밤을 이런 하나에 덮어 차 계십니다. 겨울이 써 딴은 까닭이요, 이 그리워 봅니다. 계절이 소녀들의 차 까닭입니다. 묻힌 마디씩 소학교 많은 멀듯이, 하나에 그리워 위에 버리었습니다. 까닭이요, 위에 지나고 봄이 벌레는 이웃 멀리 계십니다. 이름을 패, 그러나 릴케 하나에 별 별에도 봅니다.', point: 100, flag: 'test' } )
+db.challenges.insert({ title: 'Classic Cipher', category: 'Crypto', description: '못 까닭이요, 내 있습니다. 딴은 아무 불러 책상을 나는 멀리 시와 이런 이름과, 봅니다. 노새, 별 다 오면 남은 거외다. 지나고 가을로 파란 위에 이제 덮어 까닭입니다. 것은 이름을 때 슬퍼하는 이 묻힌 거외다. 이름과, 밤을 이런 하나에 덮어 차 계십니다. 겨울이 써 딴은 까닭이요, 이 그리워 봅니다. 계절이 소녀들의 차 까닭입니다. 묻힌 마디씩 소학교 많은 멀듯이, 하나에 그리워 위에 버리었습니다. 까닭이요, 위에 지나고 봄이 벌레는 이웃 멀리 계십니다. 이름을 패, 그러나 릴케 하나에 별 별에도 봅니다.', point: 100, flag: 'test' } )
+db.challenges.insert({ title: 'RSA', category: 'Crypto', description: '못 까닭이요, 내 있습니다. 딴은 아무 불러 책상을 나는 멀리 시와 이런 이름과, 봅니다. 노새, 별 다 오면 남은 거외다. 지나고 가을로 파란 위에 이제 덮어 까닭입니다. 것은 이름을 때 슬퍼하는 이 묻힌 거외다. 이름과, 밤을 이런 하나에 덮어 차 계십니다. 겨울이 써 딴은 까닭이요, 이 그리워 봅니다. 계절이 소녀들의 차 까닭입니다. 묻힌 마디씩 소학교 많은 멀듯이, 하나에 그리워 위에 버리었습니다. 까닭이요, 위에 지나고 봄이 벌레는 이웃 멀리 계십니다. 이름을 패, 그러나 릴케 하나에 별 별에도 봅니다.', point: 100, flag: 'test' } )
+db.challenges.insert({ title: 'babyXOR', category: 'Crypto', description: '못 까닭이요, 내 있습니다. 딴은 아무 불러 책상을 나는 멀리 시와 이런 이름과, 봅니다. 노새, 별 다 오면 남은 거외다. 지나고 가을로 파란 위에 이제 덮어 까닭입니다. 것은 이름을 때 슬퍼하는 이 묻힌 거외다. 이름과, 밤을 이런 하나에 덮어 차 계십니다. 겨울이 써 딴은 까닭이요, 이 그리워 봅니다. 계절이 소녀들의 차 까닭입니다. 묻힌 마디씩 소학교 많은 멀듯이, 하나에 그리워 위에 버리었습니다. 까닭이요, 위에 지나고 봄이 벌레는 이웃 멀리 계십니다. 이름을 패, 그러나 릴케 하나에 별 별에도 봅니다.', point: 100, flag: 'test' } )
+db.challenges.insert({ title: 'HelloCryto', category: 'Crypto', description: '못 까닭이요, 내 있습니다. 딴은 아무 불러 책상을 나는 멀리 시와 이런 이름과, 봅니다. 노새, 별 다 오면 남은 거외다. 지나고 가을로 파란 위에 이제 덮어 까닭입니다. 것은 이름을 때 슬퍼하는 이 묻힌 거외다. 이름과, 밤을 이런 하나에 덮어 차 계십니다. 겨울이 써 딴은 까닭이요, 이 그리워 봅니다. 계절이 소녀들의 차 까닭입니다. 묻힌 마디씩 소학교 많은 멀듯이, 하나에 그리워 위에 버리었습니다. 까닭이요, 위에 지나고 봄이 벌레는 이웃 멀리 계십니다. 이름을 패, 그러나 릴케 하나에 별 별에도 봅니다.', point: 100, flag: 'test' } )
