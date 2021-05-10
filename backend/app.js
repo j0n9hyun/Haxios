2c818096572e80c54923fc9ff4bd3efc07ce3341
j0n9hyun <j0n9hyun@kakao.com>
2021-05-10 14:24:16 +0900

    Development Mode

--- backend/app.js
+++ backend/app.js
@@ -5,7 +5,7 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const indexRouter = require('./routes/index');
 const mongoose = require('mongoose');
-const dotenv = require('dotenv').config();
+// const dotenv = require('dotenv').config();
 const config = require('./config/key');
 const cors = require('cors');
 const app = express();
@@ -24,19 +24,19 @@ mongoose
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
-app.use(function (req, res, next) {
-  if (!req.secure) {
-    res.redirect('https://' + haxios.kr + req.url);
-  } else {
-    next();
-  }
-});
+// app.use(function (req, res, next) {
+//   if (!req.secure) {
+//     res.redirect('https://' + haxios.kr + req.url);
+//   } else {
+//     next();
+//   }
+// });
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-// app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.static(path.join(__dirname, '../frontend/build')));
+app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.static(path.join(__dirname, '../frontend/build')));
 app.use(cors());
 app.use('/', indexRouter);
 
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -20,6 +20,10 @@ function getCurrentDate() {
   );
 }
 
+router.get('/', function (req, res) {
+  res.status(200).send('ㅎㅇ');
+});
+
 router.post('/api/users/register', function (req, res) {
   const user = new User(req.body);
   user.save((err, uesrInfo) => {
@@ -162,8 +166,8 @@ router.post('/api/users/list', auth, (req, res) => {
   );
 });
 
-router.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname, '../../frontend/build/index.html'));
-});
+// router.get('/*', function (req, res) {
+//   res.sendFile(path.join(__dirname, '../../frontend/build/index.html'));
+// });
 
 module.exports = router;
