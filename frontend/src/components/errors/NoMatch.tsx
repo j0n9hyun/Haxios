4b8bece4df57e881ab60a7ddf8d66ccf9f0fc69b
j0n9hyun <j0n9hyun@kakao.com>
2021-05-20 16:09:00 +0900

    Add Blinking Effect

--- frontend/src/components/errors/NoMatch.tsx
+++ frontend/src/components/errors/NoMatch.tsx
@@ -4,13 +4,13 @@ import title from '../../static/haxios.svg';
 const NoMatch = () => {
   return (
     <div className='notFound-Wrapper'>
-      {/* <div className='image blinking'> */}
-      <figure>
-        <img src={title} alt='' />
-      </figure>
-      <div className='notFound-contents'>잘못된 페이지</div>
+      <div className='image blinking'>
+        <figure>
+          <img src={title} alt='' />
+        </figure>
+        <div className='notFound-contents'>잘못된 페이지</div>
+      </div>
     </div>
-    // </div>
   );
 };
 
